af9d161f3ececd82fe69c65baea4170d6f18ecb9
Svetlana <svetlana@margetova.eu>
2020-05-19 16:08:25 +0200

    Update

--- oronware/wsgi.py
+++ oronware/wsgi.py
@@ -8,11 +8,22 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
-
+import time
+import traceback
+import signal
+import sys
 from django.core.wsgi import get_wsgi_application
 
-
-
+sys.path.append('/var/www/cajovnasetkani_bylinky_backend')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oronware.settings')
 
 application = get_wsgi_application()
+
+try:
+    application = get_wsgi_application()
+except Exception:
+    # Error loading applications
+    if 'mod_wsgi' in sys.modules:
+        traceback.print_exc()
+        os.kill(os.getpid(), signal.SIGINT)
+        time.sleep(2.5)
